8dff43a9e3237a1962c90666be4d69bd86487645
Cooper Christensen <cooper.c757@stu.nebo.edu>
2019-11-01 10:27:26 -0600

    Added script for left to right movement and Jumping

--- Fox On The Run/Assets/Scripts/PlayerController.cs
+++ Fox On The Run/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float horizontalInput;
+    public float speed = 7f;
+    public float zRange = 7.0f;
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+
+    private void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);  
+        }
+
+        if (transform.position.z < -zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+        }
+
+        if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.up * Time.deltaTime * horizontalInput * speed);
     }
 }
