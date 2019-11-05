4a988e46801ef89622671b8c4289efe64a356ec4
Cooper Christensen <cooper.c757@stu.nebo.edu>
2019-11-05 10:30:02 -0700

    Worked on a bit, color coded objects

--- Fox On The Run/Assets/Scripts/PlayerController.cs
+++ Fox On The Run/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float jumpForce;
     public float gravityModifier;
+    public bool isOnGround = true; 
 
     private void Start()
     {
@@ -20,9 +21,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isOnGround) 
         {
-            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);  
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         }
 
         if (transform.position.z < -zRange)
@@ -36,5 +38,9 @@ public class PlayerController : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.up * Time.deltaTime * horizontalInput * speed);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
