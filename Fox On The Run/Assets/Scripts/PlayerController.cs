514aee39e7256582287433dce750706ed94929b2
Cooper Christensen <cooper.c757@stu.nebo.edu>
2020-01-07 10:29:42 -0700

    Started switching out assets and Fixing stuff lost when scene deleted

--- Fox On The Run/Assets/Scripts/PlayerController.cs
+++ Fox On The Run/Assets/Scripts/PlayerController.cs
@@ -6,29 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 7f;
-    public float zRange = 7.0f;
+    public float xRange = 7.0f;
     private Rigidbody playerRb;
     public float jumpForce;
     public float gravityModifier;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     public bool isOnGround = true;
     public bool gameOver = false;
-=======
-    public bool isOnGround = true; 
->>>>>>> master
-=======
-    public bool isOnGround = true; 
->>>>>>> parent of 933d72d... Fixed backup error, lost some data, reworking lost data
-=======
-    public bool isOnGround = true; 
->>>>>>> parent of 933d72d... Fixed backup error, lost some data, reworking lost data
-=======
-    public bool isOnGround = true;
-    public bool gameOver = false;
->>>>>>> parent of a50a0b9... Merge remote-tracking branch 'origin/master'
 
     private void Start()
     {
@@ -39,54 +22,45 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow) && isOnGround) 
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isOnGround)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
         }
 
-        if (transform.position.z < -zRange)
+        if (transform.position.z < -xRange)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
 
-        if (transform.position.z > zRange)
+        if (transform.position.z > xRange)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.up * Time.deltaTime * horizontalInput * speed);
     }
     private void OnCollisionEnter(Collision collision)
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> parent of a50a0b9... Merge remote-tracking branch 'origin/master'
-        if(collision.gameObject.CompareTag("Ground"))
+
+        if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
         }
 
-        else if(collision.gameObject.CompareTag("Hunter"))
+        else if (collision.gameObject.CompareTag("Hunter"))
         {
             gameOver = true;
             Debug.Log("Game Over!");
         }
-       
-<<<<<<< HEAD
-=======
+
+
         isOnGround = true;
->>>>>>> master
-=======
+
+
         isOnGround = true;
->>>>>>> parent of 933d72d... Fixed backup error, lost some data, reworking lost data
-=======
+
         isOnGround = true;
->>>>>>> parent of 933d72d... Fixed backup error, lost some data, reworking lost data
-=======
->>>>>>> parent of a50a0b9... Merge remote-tracking branch 'origin/master'
+
     }
 }
